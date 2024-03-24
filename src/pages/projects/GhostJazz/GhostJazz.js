5bed12d8ccc5f5e87e6e525cccfbeb5228229800
Brandon Faunce <masterbbud@gmail.com>
2024-03-24 16:42:33 -0400

    Minor edits to ghost jazz

--- src/pages/projects/GhostJazz/GhostJazz.js
+++ src/pages/projects/GhostJazz/GhostJazz.js
@@ -11,26 +11,31 @@ function GhostJazz({ arriveAtPage }) {
     }, [])
 
     return (
-        <div className="ghostjazz-background project-background">
-          <div className="project-header">Ghost Jazz</div>
-          <div className="project-subheader">A jazz-inspired game in which a musical ghost plays his heart out to fend off the oncoming waves of skeletons at a jazz club.</div>
-          <div className="project-mainflow">
-            <div className="project-text">
-                A creation of the iconic <a href="https://itch.io/jam/rit-game-developers-club-halloween-2023-game-jam">RGDC Halloween Game Jam</a> squad consisting of <a href="mailto:tec8354@rit.edu">Tyler Combs</a>, <a href="mailto:dod4030@rit.edu">Danil Donchuk</a>, <a href="mailto:brf6174@rit.edu">Brandon Faunce</a>, and <a href="mailto:djg5455@rit.edu">Danny Gramowski</a>.
+        <div className="project-wrapper">
+            <div className="project-overlay"></div>
+            <div className="ghostjazz-background project-background">
+                
+                <div className="project-header">Ghost Jazz</div>
+                <div className="project-subheader">A jazz-inspired game in which a musical ghost plays his heart out to fend off the oncoming waves of skeletons at a jazz club.</div>
+                <div className="project-mainflow">
+                    <div className="project-text">
+                        A creation of the iconic <a href="https://itch.io/jam/rit-game-developers-club-halloween-2023-game-jam">RGDC Halloween Game Jam</a> squad consisting of <a href="mailto:tec8354@rit.edu">Tyler Combs</a>, <a href="mailto:dod4030@rit.edu">Danil Donchuk</a>, <a href="mailto:brf6174@rit.edu">Brandon Faunce</a>, and <a href="mailto:djg5455@rit.edu">Danny Gramowski</a>.
+                    </div>
+                    <div className="project-text">
+                        Project Inception: Fall 2023
+                    </div>
+                    <div className="project-image">
+                        <img src={ghostJazzTitle}></img>
+                        <div>Title Screen of Ghost Jazz</div>
+                    </div>
+                    <div className="project-text">
+                        When I heard the theme for the 2023 game jam: SPONTANEOUS, Jazz was the first thing that jumped to mind.
+                    </div>
+                </div>
+            
             </div>
-            <div className="project-text">
-                Project Inception: Fall 2023
-            </div>
-            <div className="project-image">
-                <img src={ghostJazzTitle}></img>
-                <div>Title Screen of Ghost Jazz</div>
-            </div>
-            <div className="project-text">
-                When I heard the theme for the 2023 game jam: SPONTANEOUS, Jazz was the first thing that jumped to my mind.
-            </div>
-          </div>
-          
         </div>
+        
     );
 }
 
